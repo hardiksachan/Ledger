e91af477b1ab6af25a67823c6c8f2d1bede02e9b
Hardik Sachan <sachanhardik@gmail.com>
2022-01-05 14:24:14 +0530

    added instrument vm

--- app/src/main/java/com/hardiksachan/ledger/presentation_logic/instruments/list/InstrumentsViewModel.kt
+++ app/src/main/java/com/hardiksachan/ledger/presentation_logic/instruments/list/InstrumentsViewModel.kt
@@ -1,10 +1,17 @@
 package com.hardiksachan.ledger.presentation_logic.instruments.list
 
 import com.hardiksachan.ledger.common.BackendID
+import com.hardiksachan.ledger.domain.model.Instrument
 import com.hardiksachan.ledger.domain.repository.IInstrumentRepository
+import com.hardiksachan.ledger.presentation_logic.BaseViewModel
 
 class InstrumentsViewModel(
     private val instrumentsRepo: IInstrumentRepository,
     private val navToInstrument: (BackendID) -> Unit,
-) {
+) : BaseViewModel() {
+    val instruments = instrumentsRepo.getAllInstruments()
+
+    fun onInstrumentClicked(instrument: Instrument) {
+        navToInstrument(instrument.id)
+    }
 }
